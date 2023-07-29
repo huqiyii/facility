1498df4749f9636af6053ab8751aeaf7eb7a65df
huqiyi <1523332452@qq.com>
2023-07-29 20:26:46 +0800

    add int16

--- conve/int.go
+++ conve/int.go
@@ -9,3 +9,11 @@ func Int64Default(i interface{}, v int64) int64 {
 	}
 	return value
 }
+
+func Int16Default(i interface{}, v int16) int16 {
+	value, err := conv.Int16(i)
+	if err != nil {
+		return v
+	}
+	return value
+}
